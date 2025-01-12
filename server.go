38c624f55612d1b3e88d2a0de293f5f0f0441c56
Keigo Morita <56617711+Kekechi@users.noreply.github.com>
2025-01-12 15:20:25 -0800

    local mongodb connection

--- server.go
+++ server.go
@@ -1,16 +1,70 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type SampleRadius struct {
+	Name string
+	Radius float64
+}
+
 func main() {
+
+	uri := "mongodb://localhost:27017"
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	coll := client.Database("test").Collection("sample_radius")
+
 	http.HandleFunc("GET /{$}",handlerRoot)
 	http.HandleFunc("GET /pi",handlerPI)
 	http.HandleFunc("GET /circle/{radius}",handlerCircleArea)
+
+	http.HandleFunc("GET /radius/{name}", func(w http.ResponseWriter, r *http.Request) {
+		name := r.PathValue("name")
+		var result bson.M
+		err = coll.FindOne(context.TODO(), bson.D{{"name", name}}).Decode(&result)
+		if err == mongo.ErrNoDocuments {
+			fmt.Printf("No document was found with the name %s\n", name)
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w,"%f\n", result["radius"])
+		})
+	
+	http.HandleFunc("POST /radius/", func(w http.ResponseWriter, r *http.Request) {
+		var radiusStruct SampleRadius
+		err := json.NewDecoder(r.Body).Decode(&radiusStruct)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+		radius := radiusStruct.Radius
+		name := radiusStruct.Name
+		fmt.Fprintf(w, "name = %s\n", name)
+		fmt.Fprintf(w, "radius = %f\n", radius)
+
+		_, err = coll.InsertOne(context.TODO(), bson.D{{"name", name}, {"radius", radius}})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "Document inserted successfully")
+		})
+
 	log.Fatal(http.ListenAndServe(":8000",nil))
 }
 
